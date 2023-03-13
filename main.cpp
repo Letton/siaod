b65d7cad0a72cf1fadf68e75de141c5fbb6bc32f
Letton <lettonchannel@gmail.com>
2023-03-13 22:37:21 +0300

    Task 3 Done

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ struct Word {
     char* str;
 };
 
+struct WordStr {
+    size_t firstCharIndex;
+    string str = "";
+};
+
 struct PalindromeNumberInStr {
     size_t firstCharIndex;
     size_t number;
@@ -19,9 +24,27 @@ struct PalindromeNumberInStr {
     size_t wordIndex;
 };
 
+string convertToString(char* a, size_t size)
+{
+    string s;
+    for (size_t i = 0; i < size; ++i) {
+        s.push_back(a[i]);
+    }
+    return s;
+}
+
 bool isPalindrome(const Word word) {
     for (int i = 0; i < word.size / 2; i++) {
         if (word.str[i] != word.str[word.size - i -  1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPalindrome(const WordStr word) {
+    for (int i = 0; i < word.str.size() / 2; i++) {
+        if (word.str[i] != word.str[word.str.size() - i - 1]) {
             return false;
         }
     }
@@ -61,24 +84,91 @@ void splitIntoWords(char* str, Word* words, size_t &wordsCount)  {
     }
 }
 
+void splitIntoWords(string str, vector <WordStr> &words)  {
+    size_t i = 0, j = 0;
+    while (str[i] != '\0') {
+        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))) {
+            str[i] = '\0';
+            if (j != 0) {
+                char buffer[100];
+                str.copy(buffer, j,i - j);
+                WordStr word = {
+                        i - j,
+                };
+                word.str = convertToString(buffer, j);
+                words.push_back(word);
+            }
+            j = 0;
+        } else {
+            ++j;
+        }
+        ++i;
+    }
+    if (j != 0) {
+        char buffer[100];
+        str.copy(buffer, j,i - j);
+        WordStr word = {
+                i - j,
+        };
+        word.str = convertToString(buffer, j);
+        words.push_back(word);
+    }
+}
+
 PalindromeNumberInStr findMaxPalindrome(Word *words, size_t wordsCount) {
     size_t palindromeNumber = 0;
     PalindromeNumberInStr maxPalindromeNumberInStr {
-        0,
-        0,
-        0
+            0,
+            0,
+            0
     };
     for (size_t i = 0; i < wordsCount; ++i) {
         if (isPalindrome(words[i])) {
+            ++palindromeNumber;
             if (words[i].size > maxPalindromeNumberInStr.strSize) {
                 maxPalindromeNumberInStr.firstCharIndex = words[i].firstCharIndex;
-                maxPalindromeNumberInStr.number = ++palindromeNumber;
+                maxPalindromeNumberInStr.number = palindromeNumber;
                 maxPalindromeNumberInStr.strSize = strlen(words[i].str);
                 maxPalindromeNumberInStr.wordIndex = i;
             }
         }
     }
     return maxPalindromeNumberInStr;
+}
+
+PalindromeNumberInStr findMaxPalindrome(vector <WordStr> &words) {
+    size_t palindromeNumber = 0;
+    PalindromeNumberInStr maxPalindromeNumberInStr {
+            0,
+            0,
+            0
+    };
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (isPalindrome(words[i])) {
+            ++palindromeNumber;
+            if (words[i].str.size() > maxPalindromeNumberInStr.strSize) {
+                maxPalindromeNumberInStr.firstCharIndex = words[i].firstCharIndex;
+                maxPalindromeNumberInStr.number = palindromeNumber;
+                maxPalindromeNumberInStr.strSize = words[i].str.size();
+                maxPalindromeNumberInStr.wordIndex = i;
+            }
+        }
+    }
+    return maxPalindromeNumberInStr;
+}
+
+void shiftRight(char arr[], int arrSize, int startIndex, int shiftAmount) {
+    for (int i = arrSize; i >= startIndex + shiftAmount; i--) {
+        arr[i] = arr[i - shiftAmount];
+    }
+    arr[arrSize + 1] = '\0';
+}
+
+void shiftRight(string arr, int arrSize, int startIndex, int shiftAmount) {
+    for (int i = arrSize; i >= startIndex + shiftAmount; i--) {
+        arr[i] = arr[i - shiftAmount];
+    }
+    arr[arrSize + 1] = '\0';
 }
 
 void replaceStr(char* str, PalindromeNumberInStr palindrome, Word* words) {
@@ -92,18 +182,66 @@ void replaceStr(char* str, PalindromeNumberInStr palindrome, Word* words) {
         tempSize++;
     }
     tempStr[tempSize] = '\0';
-    cout << tempStr << " " << tempSize;
+    if (tempSize <= wordToReplace.size) {
+        for (size_t i = 0; i <  tempSize; ++i) {
+            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
+        }
+        for (size_t i = 0; i <  wordToReplace.size - tempSize; ++i) {
+            for (size_t j = wordToReplace.firstCharIndex + tempSize; j < strlen(str); ++j) {
+                str[j] = str[j + 1];
+            }
+        }
+    } else {
+        for (size_t i = 0; i < tempSize - wordToReplace.size; ++i) {
+            shiftRight(str, strlen(str), wordToReplace.firstCharIndex + wordToReplace.size,
+                       1);
+        }
+        for (size_t i = 0; i < tempSize; ++i) {
+            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
+        }
+    }
+}
+
+void replaceStr(string &str, PalindromeNumberInStr palindrome, vector <WordStr> words) {
+    WordStr wordToReplace = words[palindrome.wordIndex];
+    char tempStr [1024];
+    size_t tempSize = 0;
+    size_t tempNumber = palindrome.number;
+    while (tempNumber != 0) {
+        tempStr[tempSize] = (tempNumber % 10) + '0';
+        tempNumber /= 10;
+        tempSize++;
+    }
+    tempStr[tempSize] = '\0';
+    if (tempSize <= wordToReplace.str.size()) {
+        for (size_t i = 0; i <  tempSize; ++i) {
+            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
+        }
+        for (size_t i = 0; i <  wordToReplace.str.size() - tempSize; ++i) {
+            for (size_t j = wordToReplace.firstCharIndex + tempSize; j < str.size(); ++j) {
+                str[j] = str[j + 1];
+            }
+        }
+    } else {
+        for (size_t i = 0; i < tempSize - wordToReplace.str.size(); ++i) {
+            shiftRight(str, str.size(), wordToReplace.firstCharIndex + wordToReplace.str.size(),
+                       1);
+        }
+        for (size_t i = 0; i < tempSize; ++i) {
+            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
+        }
+    }
 }
 
 int main() {
     int n;
-    cout << "Select task: \n"
-            "1 - Given a rectangular matrix. Rearrange the reduced element of the matrix in its upper left corner,\n"
-            "    sequentially permuting rows and columns.\n"
+    cout << "1 - Given a sentence consisting of words separated by commas or spaces. Find the longest palindrome word.\n"
+            "    Replace a word in a sentence with a number given its number among the sentence's palindromes.\n"
             "    (static array)\n"
-            "2 - Given a rectangular matrix. Rearrange the reduced element of the matrix in its upper left corner,\n"
-            "    sequentially permuting rows and columns.\n"
+            "2 - Given a sentence consisting of words separated by commas or spaces. Find the longest palindrome word.\n"
+            "    Replace a word in a sentence with a number given its number among the sentence's palindromes.\n"
             "    (std::string)\n";
+    cout << "Select task:\n";
     cin >> n;
     if (n == 1) {
         size_t wordsCount = 0;
@@ -115,8 +253,16 @@ int main() {
         copyCharArray(tempStr, str);
         splitIntoWords(tempStr, words, wordsCount);
         replaceStr(str, findMaxPalindrome(words, wordsCount), words);
+        cout << str;
     } else if (n == 2) {
-
+        string str;
+        vector <WordStr> words;
+        cout << "Input sentence\n";
+        cin.ignore();
+        getline(cin, str);
+        splitIntoWords(str, words);
+        replaceStr(str, findMaxPalindrome(words), words);
+        cout << str;
     } else {
         cout << "Incorrect input\n";
         return -1;
