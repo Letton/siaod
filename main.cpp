02ec1a89c020236a35e60b3788f0400626f84ec5
Letton <lettonchannel@gmail.com>
2023-03-17 10:05:03 +0300

    Task 4

--- main.cpp
+++ main.cpp
@@ -1,271 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <cstring>
-
-using namespace std;
-
-struct Word {
-    size_t firstCharIndex;
-    size_t size;
-    char* str;
-};
-
-struct WordStr {
-    size_t firstCharIndex;
-    string str = "";
-};
-
-struct PalindromeNumberInStr {
-    size_t firstCharIndex;
-    size_t number;
-    size_t strSize;
-    size_t wordIndex;
-};
-
-string convertToString(char* a, size_t size)
-{
-    string s;
-    for (size_t i = 0; i < size; ++i) {
-        s.push_back(a[i]);
-    }
-    return s;
-}
-
-bool isPalindrome(const Word word) {
-    for (int i = 0; i < word.size / 2; i++) {
-        if (word.str[i] != word.str[word.size - i -  1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool isPalindrome(const WordStr word) {
-    for (int i = 0; i < word.str.size() / 2; i++) {
-        if (word.str[i] != word.str[word.str.size() - i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void copyCharArray(char *dest, char *src) {
-    size_t len = strlen(src);
-    for (size_t i = 0; i < len; i++) {
-        dest[i] = src[i];
-    }
-    dest[len] = '\0';
-}
-
-void splitIntoWords(char* str, Word* words, size_t &wordsCount)  {
-    size_t i = 0, j = 0;
-    while (str[i] != '\0') {
-        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))) {
-            str[i] = '\0';
-            if (j != 0) {
-                words[wordsCount].str = &str[i - j];
-                words[wordsCount].size = j;
-                words[wordsCount].firstCharIndex  = i - j;
-                wordsCount += 1;
-            }
-            j = 0;
-        } else {
-            ++j;
-        }
-        ++i;
-    }
-    if (j != 0) {
-        words[wordsCount].str = &str[i - j];
-        words[wordsCount].size = j;
-        words[wordsCount].firstCharIndex  = i - j;
-        wordsCount += 1;
-    }
-}
-
-void splitIntoWords(string str, vector <WordStr> &words)  {
-    size_t i = 0, j = 0;
-    while (str[i] != '\0') {
-        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))) {
-            str[i] = '\0';
-            if (j != 0) {
-                char buffer[100];
-                str.copy(buffer, j,i - j);
-                WordStr word = {
-                        i - j,
-                };
-                word.str = convertToString(buffer, j);
-                words.push_back(word);
-            }
-            j = 0;
-        } else {
-            ++j;
-        }
-        ++i;
-    }
-    if (j != 0) {
-        char buffer[100];
-        str.copy(buffer, j,i - j);
-        WordStr word = {
-                i - j,
-        };
-        word.str = convertToString(buffer, j);
-        words.push_back(word);
-    }
-}
-
-PalindromeNumberInStr findMaxPalindrome(Word *words, size_t wordsCount) {
-    size_t palindromeNumber = 0;
-    PalindromeNumberInStr maxPalindromeNumberInStr {
-            0,
-            0,
-            0
-    };
-    for (size_t i = 0; i < wordsCount; ++i) {
-        if (isPalindrome(words[i])) {
-            ++palindromeNumber;
-            if (words[i].size > maxPalindromeNumberInStr.strSize) {
-                maxPalindromeNumberInStr.firstCharIndex = words[i].firstCharIndex;
-                maxPalindromeNumberInStr.number = palindromeNumber;
-                maxPalindromeNumberInStr.strSize = strlen(words[i].str);
-                maxPalindromeNumberInStr.wordIndex = i;
-            }
-        }
-    }
-    return maxPalindromeNumberInStr;
-}
-
-PalindromeNumberInStr findMaxPalindrome(vector <WordStr> &words) {
-    size_t palindromeNumber = 0;
-    PalindromeNumberInStr maxPalindromeNumberInStr {
-            0,
-            0,
-            0
-    };
-    for (size_t i = 0; i < words.size(); ++i) {
-        if (isPalindrome(words[i])) {
-            ++palindromeNumber;
-            if (words[i].str.size() > maxPalindromeNumberInStr.strSize) {
-                maxPalindromeNumberInStr.firstCharIndex = words[i].firstCharIndex;
-                maxPalindromeNumberInStr.number = palindromeNumber;
-                maxPalindromeNumberInStr.strSize = words[i].str.size();
-                maxPalindromeNumberInStr.wordIndex = i;
-            }
-        }
-    }
-    return maxPalindromeNumberInStr;
-}
-
-void shiftRight(char arr[], int arrSize, int startIndex, int shiftAmount) {
-    for (int i = arrSize; i >= startIndex + shiftAmount; i--) {
-        arr[i] = arr[i - shiftAmount];
-    }
-    arr[arrSize + 1] = '\0';
-}
-
-void shiftRight(string arr, int arrSize, int startIndex, int shiftAmount) {
-    for (int i = arrSize; i >= startIndex + shiftAmount; i--) {
-        arr[i] = arr[i - shiftAmount];
-    }
-    arr[arrSize + 1] = '\0';
-}
-
-void replaceStr(char* str, PalindromeNumberInStr palindrome, Word* words) {
-    Word wordToReplace = words[palindrome.wordIndex];
-    char tempStr [1024];
-    size_t tempSize = 0;
-    size_t tempNumber = palindrome.number;
-    while (tempNumber != 0) {
-        tempStr[tempSize] = (tempNumber % 10) + '0';
-        tempNumber /= 10;
-        tempSize++;
-    }
-    tempStr[tempSize] = '\0';
-    if (tempSize <= wordToReplace.size) {
-        for (size_t i = 0; i <  tempSize; ++i) {
-            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
-        }
-        for (size_t i = 0; i <  wordToReplace.size - tempSize; ++i) {
-            for (size_t j = wordToReplace.firstCharIndex + tempSize; j < strlen(str); ++j) {
-                str[j] = str[j + 1];
-            }
-        }
-    } else {
-        for (size_t i = 0; i < tempSize - wordToReplace.size; ++i) {
-            shiftRight(str, strlen(str), wordToReplace.firstCharIndex + wordToReplace.size,
-                       1);
-        }
-        for (size_t i = 0; i < tempSize; ++i) {
-            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
-        }
-    }
-}
-
-void replaceStr(string &str, PalindromeNumberInStr palindrome, vector <WordStr> words) {
-    WordStr wordToReplace = words[palindrome.wordIndex];
-    char tempStr [1024];
-    size_t tempSize = 0;
-    size_t tempNumber = palindrome.number;
-    while (tempNumber != 0) {
-        tempStr[tempSize] = (tempNumber % 10) + '0';
-        tempNumber /= 10;
-        tempSize++;
-    }
-    tempStr[tempSize] = '\0';
-    if (tempSize <= wordToReplace.str.size()) {
-        for (size_t i = 0; i <  tempSize; ++i) {
-            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
-        }
-        for (size_t i = 0; i <  wordToReplace.str.size() - tempSize; ++i) {
-            for (size_t j = wordToReplace.firstCharIndex + tempSize; j < str.size(); ++j) {
-                str[j] = str[j + 1];
-            }
-        }
-    } else {
-        for (size_t i = 0; i < tempSize - wordToReplace.str.size(); ++i) {
-            shiftRight(str, str.size(), wordToReplace.firstCharIndex + wordToReplace.str.size(),
-                       1);
-        }
-        for (size_t i = 0; i < tempSize; ++i) {
-            str[wordToReplace.firstCharIndex + i] = tempStr[tempSize - i - 1];
-        }
-    }
-}
+#include "struct.h"
 
 int main() {
+    Table table = {};
     int n;
-    cout << "1 - Given a sentence consisting of words separated by commas or spaces. Find the longest palindrome word.\n"
-            "    Replace a word in a sentence with a number given its number among the sentence's palindromes.\n"
-            "    (static array)\n"
-            "2 - Given a sentence consisting of words separated by commas or spaces. Find the longest palindrome word.\n"
-            "    Replace a word in a sentence with a number given its number among the sentence's palindromes.\n"
-            "    (std::string)\n";
-    cout << "Select task:\n";
+    cout << "Select option: \n"
+            "1 - Fill the table with test data\n"
+            "2 - Select task\n"
+            "3 - Exit\n";
     cin >> n;
     if (n == 1) {
-        size_t wordsCount = 0;
-        char str[1024], tempStr[1024];
-        Word words[1024];
-        cout << "Input sentence (max sentence size = 1024)\n";
-        cin.ignore();
-        cin.getline(str, 1024);
-        copyCharArray(tempStr, str);
-        splitIntoWords(tempStr, words, wordsCount);
-        replaceStr(str, findMaxPalindrome(words, wordsCount), words);
-        cout << str;
-    } else if (n == 2) {
-        string str;
-        vector <WordStr> words;
-        cout << "Input sentence\n";
-        cin.ignore();
-        getline(cin, str);
-        splitIntoWords(str, words);
-        replaceStr(str, findMaxPalindrome(words), words);
-        cout << str;
-    } else {
-        cout << "Incorrect input\n";
-        return -1;
+        push_back(table, {"090302", "Applied_Informatics",
+                          "090302_1", "Data_management", 2022});
+        push_back(table, {"090304", "Software_engineering",
+                          "090304_1", "OOP", 2023});
+        push_back(table, {"090304", "Software_engineering",
+                          "090304_2", "Mobile_applications", 2024});
+        push_back(table, {"090304", "Software_engineering",
+                          "090304_3", "Mathematical_analysis", 2023});
+        push_back(table, {"090304", "Software_engineering",
+                          "090304_4", "Linear_algebra", 2018});
+        cout << table;
+    }
+    while (n != 0) {
+        cout << "Select task: \n"
+                "1 - Fill out an entry for one curriculum\n"
+                "2 - Insert an entry in the plan table at the beginning of the sublist, with the same direction cipher\n"
+                "3 - Display the names of the profiles and the start year of the curriculum of the given direction\n"
+                "4 - Delete plans that have expired after 4 years\n"
+                "0 - Exit\n";
+        cin >> n;
+        if (n == 1) {
+            Curriculum curriculum = {};
+            cin >> curriculum;
+            push_back(table, curriculum);
+            cout << table;
+        }
+        if (n == 2) {
+            Curriculum curriculum = {};
+            cin >> curriculum;
+            insert(table, curriculum);
+            cout << table;
+        }
+        if (n == 3) {
+            cout << "Input Direction cipher\n";
+            char direction_cipher[100];
+            cin >> direction_cipher;
+            find_curriculum(table, direction_cipher);
+        }
+        if (n == 4) {
+            delete_on_expiration(table);
+            cout << table;
+        }
     }
     return 0;
 }
---
+++ struct.cpp
@@ -0,0 +1,107 @@
+#include "struct.h"
+#include <iomanip>
+#include <ctime>
+
+ostream &operator<<(ostream &os, const Table &table) {
+    cout.setf(ios::left);
+    cout << setw(20) << "Direction cipher" << " | ";
+    cout << setw(25) << "Direction title" << " | ";
+    cout << setw(20) << "Profile cipher" << " | ";
+    cout << setw(25) << "Profile title" << " | ";
+    cout << setw(10) << "Start year" << "\n";
+    cout << setw(112) << setfill('-') << "" << "\n";
+    for (size_t i = 0; i < table.size; ++i) {
+        cout << setw(20) << setfill(' ') << table.rows[i].direction_cipher << " | ";
+        cout << setw(25) << table.rows[i].direction_title << " | ";
+        cout << setw(20) << table.rows[i].profile_cipher << " | ";
+        cout << setw(25) << table.rows[i].profile_title << " | ";
+        cout << setw(10) << table.rows[i].start_year << "\n";
+    }
+    return os;
+}
+
+istream &operator>>(istream &is, Curriculum &curriculum) {
+    cout << "Input Direction cipher, Direction title, Profile cipher, Profile title, Start year, "
+            "use \"space\" as delimiter\n";
+    cout << "Example: 090304 Software_engineering 090304_2 Mobile_applications 2024\n";
+    is >> curriculum.direction_cipher >> curriculum.direction_title >> curriculum.profile_cipher;
+    is >> curriculum.profile_title >> curriculum.start_year;
+    return is;
+}
+
+void push_back(Table &table, const Curriculum &curriculum) {
+    table.size++;
+    auto *resized = new Curriculum[table.size];
+    for (size_t i = 0; i < table.size - 1; ++i) {
+        resized[i] = table.rows[i];
+    }
+    resized[table.size - 1] = curriculum;
+    delete[] table.rows;
+    table.rows = resized;
+}
+
+void insert(Table &table, const Curriculum &curriculum) {
+    size_t insert_index = -1;
+    for (size_t i = 0; i < table.size; ++i) {
+        if (strcmp(table.rows[i].direction_cipher, curriculum.direction_cipher) == 0) {
+            insert_index = i;
+            break;
+        }
+    }
+    if (insert_index == -1) {
+        cout << "No cipher found\n";
+        return;
+    }
+    table.size++;
+    auto *resized = new Curriculum[table.size];
+    for (size_t i = 0; i < insert_index; ++i) {
+        resized[i] = table.rows[i];
+    }
+    resized[insert_index] = curriculum;
+    for (size_t i = insert_index + 1; i < table.size; ++i) {
+        resized[i] = table.rows[i - 1];
+    }
+    delete[] table.rows;
+    table.rows = resized;
+}
+
+void find_curriculum(Table &table, char direction_cipher[100]) {
+    bool flag = false;
+    for (size_t i = 0; i < table.size; ++i) {
+        if (strcmp(table.rows[i].direction_cipher, direction_cipher) == 0) {
+            if (!flag) {
+                cout << setw(25) << "Profile title" << " | ";
+                cout << setw(10) << "Start year" << "\n";
+                cout << setw(38) << setfill('-') << "" << "\n";
+                flag = true;
+            }
+            cout << setw(25) << setfill(' ') << table.rows[i].profile_title << " | ";
+            cout << setw(10) << table.rows[i].start_year << "\n";
+        }
+    }
+    if (!flag) {
+        cout << "No cipher found\n";
+    }
+}
+
+void delete_on_expiration(Table &table) {
+    time_t now = time(nullptr);
+    tm ltm = {};
+    localtime_s(&ltm, &now);
+    cout << ltm.tm_year + 1900;
+    for (size_t i = 0; i < table.size; ++i) {
+        if (table.rows[i].start_year < ltm.tm_year + 1900 - 3) {
+            table.size--;
+            auto *resized = new Curriculum[table.size];
+            for (size_t j = 0; j < i; ++j) {
+                resized[j] = table.rows[j];
+            }
+            for (size_t j = i; j < table.size ; ++j) {
+                resized[j] = table.rows[j + 1];
+            }
+            delete[] table.rows;
+            table.rows = resized;
+            --i;
+        }
+    }
+}
